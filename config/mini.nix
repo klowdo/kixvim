2c8f98bb4d9ed42ab46dd67e8990fa5c3d3f0c6a
Felix Svensson <klowdo.fs@gmail.com>
2025-02-17 22:42:09 +0100

    feat(mini.files): open current file path

--- config/mini.nix
+++ config/mini.nix
@@ -40,7 +40,7 @@
   keymaps = [
     {
       key = "<leader>e";
-      action = "<CMD>lua MiniFiles.open()<CR>";
+      action = ":lua OpenMiniFileCWD()<cr>";
       options = {
         desc = "[E]xpore (Mini.files)";
       };
@@ -52,6 +52,15 @@
   # cursor location to LINE:COLUMN
   # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraconfiglu#extraconfiglua
   extraConfigLua = ''
+     function OpenMiniFileCWD()
+      local MiniFiles = require("mini.files")
+      local _ = MiniFiles.close()
+        or MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+      vim.defer_fn(function()
+        MiniFiles.reveal_cwd()
+      end, 30)
+     end
+
     require('mini.statusline').section_location = function()
       return '%2l:%-2v'
     end
