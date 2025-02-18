e5337585ecf8f7bcbc7d3c447353706398c48e34
Felix Svensson <klowdo.fs@gmail.com>
2025-02-18 21:47:48 +0100

    feat(harpoon): add config

--- config/plugins/custom/plugins/harpoon.nix
+++ config/plugins/custom/plugins/harpoon.nix
@@ -1,6 +1,20 @@
 {
-    plugins.harpoon = {
-      enable = true;
-      enableTelescope = true;
+  plugins.harpoon = {
+    enable = true;
+    enableTelescope = true;
+    keymapsSilent = true;
+
+    saveOnChange = true;
+    saveOnToggle = true;
+    keymaps = {
+      addFile = "<leader>ha";
+      toggleQuickMenu = "<C-e>";
+      navFile = {
+        "1" = "<leader>hj";
+        "2" = "<leader>hk";
+        "3" = "<leader>hl";
+        "4" = "<leader>hm";
+      };
     };
+  };
 }
