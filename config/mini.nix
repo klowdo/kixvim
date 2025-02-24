55defd445b50054915db0068e63e7a33cd61d5cd
Felix Svensson <klowdo.fs@gmail.com>
2025-02-24 15:47:30 +0100

    feat(mini.statusline): add recording info

--- config/mini.nix
+++ config/mini.nix
@@ -51,6 +51,7 @@
   # default behavior. For example, here we set the section for
   # cursor location to LINE:COLUMN
   # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraconfiglu#extraconfiglua
+  # https://www.reddit.com/r/neovim/comments/1djkwif/show_recording_macros_message_in_ministatusline/
   extraConfigLua = ''
      function OpenMiniFileCWD()
       local MiniFiles = require("mini.files")
@@ -61,8 +62,41 @@
       end, 30)
      end
 
-    require('mini.statusline').section_location = function()
-      return '%2l:%-2v'
-    end
+    require("mini.statusline").setup({
+      use_icons = vim.g.have_nerd_font,
+      content = {
+        active = function()
+          local check_macro_recording = function()
+            if vim.fn.reg_recording() ~= "" then
+              return "Recording @" .. vim.fn.reg_recording()
+            else
+              return ""
+            end
+          end
+
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local git = MiniStatusline.section_git({ trunc_width = 40 })
+          local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+          local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+          -- local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+          local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local location = MiniStatusline.section_location({ trunc_width = 200 })
+          local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+          local macro = check_macro_recording()
+
+          return MiniStatusline.combine_groups({
+            { hl = mode_hl, strings = { mode } },
+            { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics } },
+            "%<", -- Mark general truncate point
+            { hl = "MiniStatuslineFilename", strings = { filename } },
+            "%=", -- End left alignment
+            { hl = "MiniStatuslineFilename", strings = { macro } },
+            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+            { hl = mode_hl, strings = { search, location } },
+          })
+        end,
+      },
+    })
   '';
 }
