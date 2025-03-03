1ad22ec889dabc2d891ba506f233a5b967dc49bf
Felix Svensson <klowdo.fs@gmail.com>
2025-03-03 14:34:08 +0100

    fix(flash): enable bindings for flash

--- config/plugins/flash.nix
+++ config/plugins/flash.nix
@@ -2,15 +2,37 @@
   plugins.flash = {
     enable = true;
   };
-  # keymaps = [
 
-  # {key "s";mode = [ "n", "x", "o" ], function() require("flash").jump() end, desc = "Flash" },
-  # {key "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-  # {key "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-  # {key "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-  # {key "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  # },
-  # }
-  #
-  #   ];
+  keymaps = [
+    {
+      key = "s";
+      mode = ["n" "x" "o"];
+      action.__raw = ''function() require("flash").jump() end'';
+      options = {desc = "Flash";};
+    }
+    {
+      key = "S";
+      mode = ["n" "x" "o"];
+      action.__raw = ''function() require("flash").treesitter() end'';
+      options = {desc = "Flash Treesitter";};
+    }
+    {
+      key = "r";
+      mode = "o";
+      action.__raw = ''function() require("flash").remote() end'';
+      options = {desc = "Remote Flash";};
+    }
+    {
+      key = "R";
+      mode = ["o" "x"];
+      action.__raw = ''function() require("flash").treesitter_search() end'';
+      options = {desc = "Treesitter Search";};
+    }
+    {
+      key = "<c-s>";
+      mode = ["c"];
+      action.__raw = ''function() require("flash").toggle() end'';
+      options = {desc = "Toggle Flash Search";};
+    }
+  ];
 }
