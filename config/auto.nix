9158383e9b8c2d7e76f830d729f9e6b7a290796d
Felix Svensson <klowdo.fs@gmail.com>
2025-03-16 17:06:13 +0100

    chore(auto): add auto-save

--- config/auto.nix
+++ config/auto.nix
@@ -6,6 +6,25 @@
     };
   };
 
+  plugins.auto-save = {
+    enable = true;
+    settings = {
+      condition = ''
+        function(buf)
+          local fn = vim.fn
+          local utils = require("auto-save.utils.data")
+
+          if utils.not_in(fn.getbufvar(buf, "&filetype"), {'oil'}) then
+            return true
+          end
+          return false
+        end
+      '';
+      debounce_delay = 1000;
+      write_all_buffers = true;
+    };
+  };
+
   # [[ Basic Autocommands ]]
   #  See `:help lua-guide-autocommands`
   # https://nix-community.github.io/nixvim/NeovimOptions/autoCmd/index.html
