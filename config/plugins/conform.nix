bf2044fb63e46f86389ecca1039cb2474084eb49
Felix Svensson <klowdo.fs@gmail.com>
2025-05-30 22:14:11 +0200

    fix(conform): use default config

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -40,33 +40,36 @@
         # Lua
         ''
           function(bufnr)
-              -- Disable autoformat on certain filetypes
-              local ignore_filetypes = { "sql", "java" }
-              if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-                return
-              end
-              -- Disable with a global or buffer-local variable
-              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-              end
-              -- Disable autoformat for files in a certain path
-              local bufname = vim.api.nvim_buf_get_name(bufnr)
-              if bufname:match("/node_modules/") then
-                return
-              end
-             -- ...additional logic...
-              return { timeout_ms = 500, lsp_format = "fallback" }
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
             end
+
+            if slow_format_filetypes[vim.bo[bufnr].filetype] then
+              return
+            end
+
+            local function on_format(err)
+              if err and err:match("timeout$") then
+                slow_format_filetypes[vim.bo[bufnr].filetype] = true
+              end
+            end
+
+            return { timeout_ms = 200, lsp_fallback = true }, on_format
+           end
         '';
       format_after_save =
         # Lua
         ''
-           function(bufnr)
+          function(bufnr)
             if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
               return
             end
-            -- ...additional logic...
-            return { lsp_format = "fallback" }
+
+            if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+              return
+            end
+
+            return { lsp_fallback = true }
           end
         '';
       log_level = "warn";
