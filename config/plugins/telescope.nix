19c84e64cb9206d4d6fad4a492ac51017dd06948
Felix Svensson <klowdo.fs@gmail.com>
2025-05-30 22:48:53 +0200

    feat(telescope): add mainix and keymaps

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -1,4 +1,7 @@
 {
+  imports = [
+    ./telescope/manix.nix
+  ];
   # Fuzzy Finder (files, lsp, etc)
   # https://nix-community.github.io/nixvim/plugins/telescope/index.html
   plugins.telescope = {
@@ -33,78 +36,82 @@
 
     #  # You can put your default mappings / updates / etc. in here
     #  #  See `:help telescope.builtin`
-    # keymaps = {
-    #    "<leader>sh" = {
-    #      mode = "n";
-    #      action = "help_tags";
-    #      options = {
-    #        desc = "[S]earch [H]elp";
-    #      };
-    #    };
-    #    "<leader>sk" = {
-    #      mode = "n";
-    #      action = "keymaps";
-    #      options = {
-    #        desc = "[S]earch [K]eymaps";
-    #      };
-    #    };
-    #    "<leader>sf" = {
-    #      mode = "n";
-    #      action = "find_files";
-    #      options = {
-    #        desc = "[S]earch [F]iles";
-    #      };
-    #    };
-    #    "<leader>ss" = {
-    #      mode = "n";
-    #      action = "builtin";
-    #      options = {
-    #        desc = "[S]earch [S]elect Telescope";
-    #      };
-    #    };
-    #    "<leader>sw" = {
-    #      mode = "n";
-    #      action = "grep_string";
-    #      options = {
-    #        desc = "[S]earch current [W]ord";
-    #      };
-    #    };
-    #    "<leader>sg" = {
-    #      mode = "n";
-    #      action = "live_grep";
-    #      options = {
-    #        desc = "[S]earch by [G]rep";
-    #      };
-    #    };
-    #    "<leader>sd" = {
-    #      mode = "n";
-    #      action = "diagnostics";
-    #      options = {
-    #        desc = "[S]earch [D]iagnostics";
-    #      };
-    #    };
-    #    "<leader>sr" = {
-    #      mode = "n";
-    #      action = "resume";
-    #      options = {
-    #        desc = "[S]earch [R]esume";
-    #      };
-    #    };
-    #    "<leader>s" = {
-    #      mode = "n";
-    #      action = "oldfiles";
-    #      options = {
-    #        desc = "[S]earch Recent Files ('.' for repeat)";
-    #      };
-    #    };
-    #    "<leader><leader>" = {
-    #      mode = "n";
-    #      action = "buffers";
-    #      options = {
-    #        desc = "[ ] Find existing buffers";
-    #      };
-    #    };
-    #  };
+    keymaps = {
+      # "<leader>sh" = {
+      #   mode = "n";
+      #   action = "help_tags";
+      #   options = {
+      #     desc = "[S]earch [H]elp";
+      #   };
+      # };
+      "<leader>sk" = {
+        mode = "n";
+        action = "keymaps";
+        options = {
+          desc = "[S]earch [K]eymaps";
+        };
+      };
+      "<leader>sf" = {
+        mode = "n";
+        action = "find_files";
+        options = {
+          desc = "[S]earch [F]iles";
+        };
+      };
+      "<leader>ss" = {
+        mode = "n";
+        action = "builtin";
+        options = {
+          desc = "[S]earch [S]elect Telescope";
+        };
+      };
+      "<leader>sw" = {
+        mode = "n";
+        action = "grep_string";
+        options = {
+          desc = "[S]earch current [W]ord";
+        };
+      };
+      "<leader>sg" = {
+        mode = "n";
+        action = "live_grep";
+        options = {
+          desc = "[S]earch by [G]rep";
+        };
+      };
+      "<leader>sd" = {
+        mode = "n";
+        action = "diagnostics";
+        options = {
+          desc = "[S]earch [D]iagnostics";
+        };
+      };
+      "<leader>sr" = {
+        mode = "n";
+        action = "resume";
+        options = {
+          desc = "[S]earch [R]esume";
+        };
+      };
+      "<leader>s" = {
+        mode = "n";
+        action = "oldfiles";
+        options = {
+          desc = "[S]earch Recent Files ('.' for repeat)";
+        };
+      };
+      "<leader><leader>" = {
+        mode = "n";
+        action = "buffers";
+        options = {
+          desc = "[ ] Find existing buffers";
+        };
+      };
+      "<leader>fs" = {
+        action = "lsp_document_symbols";
+        options.desc = "Search symbols";
+      };
+    };
     settings = {
       extensions.__raw = "{ ['ui-select'] = { require('telescope.themes').get_dropdown() } }";
     };
