8077c302df3bea026679f89324d4070fc26561ca
Felix Svensson <klowdo.fs@gmail.com>
2025-05-30 22:48:00 +0200

    fix(conform): add missing table

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -16,6 +16,49 @@
   # https://nix-community.github.io/nixvim/plugins/conform-nvim.html
   plugins.conform-nvim = {
     enable = true;
+
+    luaConfig.pre =
+      # lua
+      ''
+        local slow_format_filetypes = {}
+      '';
+
+    # TODO: Can this be handled by https://github.com/folke/snacks.nvim/blob/main/docs/toggle.md?
+    luaConfig.content =
+      # lua
+      ''
+        vim.api.nvim_create_user_command("FormatDisable", function(args)
+           if args.bang then
+            -- FormatDisable! will disable formatting just for this buffer
+            vim.b.disable_autoformat = true
+          else
+            vim.g.disable_autoformat = true
+          end
+        end, {
+          desc = "Disable autoformat-on-save",
+          bang = true,
+        })
+
+        vim.api.nvim_create_user_command("FormatEnable", function()
+          vim.b.disable_autoformat = false
+          vim.g.disable_autoformat = false
+        end, {
+          desc = "Re-enable autoformat-on-save",
+        })
+
+        vim.api.nvim_create_user_command("FormatToggle", function(args)
+          if args.bang then
+            -- Toggle formatting for current buffer
+            vim.b.disable_autoformat = not vim.b.disable_autoformat
+          else
+            -- Toggle formatting globally
+            vim.g.disable_autoformat = not vim.g.disable_autoformat
+          end
+        end, {
+          desc = "Toggle autoformat-on-save",
+          bang = true,
+        })
+      '';
     settings = {
       formatters_by_ft = {
         lua = ["stylua"];
