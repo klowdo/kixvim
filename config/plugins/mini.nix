69e806e19662b94ad6ceba9df8cb6bb7de2c7fb0
Felix Svensson <klowdo.fs@gmail.com>
2025-06-04 10:34:24 +0200

    fix(mini): consolidate to one file and fix opening issue

--- config/plugins/mini.nix
+++ config/plugins/mini.nix
@@ -40,7 +40,7 @@
   keymaps = [
     {
       key = "<leader>e";
-      action = "<CMD>lua MiniFiles.open()<CR>";
+      action = ":lua OpenMiniFileCWD()<cr>";
       options = {
         desc = "[E]xpore (Mini.files)";
       };
@@ -55,5 +55,74 @@
     require('mini.statusline').section_location = function()
       return '%2l:%-2v'
     end
+
+     function OpenMiniFileCWD()
+        local MiniFiles = require("mini.files")
+
+        -- Check if current buffer is a terminal
+        local function is_terminal_buffer()
+          local buftype = vim.api.nvim_buf_get_option(0, 'buftype')
+          return buftype == 'terminal'
+        end
+
+        -- Get appropriate path for mini.files
+        local function get_minifiles_path()
+          if is_terminal_buffer() then
+            -- For terminal buffers, use current working directory
+            return vim.fn.getcwd()
+          else
+            -- For regular files, use the buffer's file path
+            local buf_name = vim.api.nvim_buf_get_name(0)
+            -- If buffer has no name (empty buffer), use cwd
+            if buf_name == "" then
+              return vim.fn.getcwd()
+            end
+            return buf_name
+          end
+        end
+
+        local _ = MiniFiles.close() or MiniFiles.open(get_minifiles_path(), false)
+
+        vim.defer_fn(function()
+          MiniFiles.reveal_cwd()
+        end, 30)
+     end
+
+    require("mini.statusline").setup({
+      use_icons = vim.g.have_nerd_font,
+      content = {
+        active = function()
+          local check_macro_recording = function()
+            if vim.fn.reg_recording() ~= "" then
+              return "Recording @" .. vim.fn.reg_recording()
+            else
+              return ""
+            end
+          end
+
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local git = MiniStatusline.section_git({ trunc_width = 40 })
+          local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+          local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+          -- local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+          local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local location = MiniStatusline.section_location({ trunc_width = 200 })
+          local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+          local macro = check_macro_recording()
+
+          return MiniStatusline.combine_groups({
+            { hl = mode_hl, strings = { mode } },
+            { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics } },
+            "%<", -- Mark general truncate point
+            { hl = "MiniStatuslineFilename", strings = { filename } },
+            "%=", -- End left alignment
+            { hl = "MiniStatuslineFilename", strings = { macro } },
+            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+            { hl = mode_hl, strings = { search, location } },
+          })
+        end,
+      },
+    })
   '';
 }
