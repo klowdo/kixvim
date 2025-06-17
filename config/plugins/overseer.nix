5ebfe1302431662da75eb70342694113c73e427f
Felix Svensson <klowdo.fs@gmail.com>
2025-06-17 09:28:59 +0200

    feat(plugins): add overseer

---
+++ config/plugins/overseer.nix
@@ -0,0 +1,13 @@
+{
+  plugins.overseer = {
+    enable = true;
+    settings = {
+      task_list = {
+        direction = "bottom";
+        min_height = 25;
+        max_height = 25;
+        default_detail = 1;
+      };
+    };
+  };
+}
