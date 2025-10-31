53ca0f78d4aff1ba3a576c6433cb96b02db956f8
Felix Svensson <klowdo.fs@gmail.com>
2025-10-31 10:11:30 +0100

    fix(golang/lint): fix quickfix format

--- config/plugins/golangci-lint.nix
+++ config/plugins/golangci-lint.nix
@@ -1,8 +1,8 @@
 {
   extraConfigLua = ''
     vim.api.nvim_create_user_command('GolangciLint', function()
-      vim.cmd('setlocal makeprg=golangci-lint\\ run\\ --output.tab.path=stdout\\ --show-stats=false')
-      vim.cmd('setlocal errorformat=%f:%l:%c\\ %m')
+      vim.cmd('setlocal makeprg=golangci-lint\\ run\\ --output.text.path=stdout\\ --output.text.colors=false')
+      vim.cmd('setlocal errorformat=%f:%l:%c:\\ %m,%f:%l:\\ %m,%-G%.%#')
       vim.cmd('make!')
       vim.cmd('copen')
     end, {})
